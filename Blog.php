540891ac602eb64906a4105c27e7257609e52780
dpat <davidpat.sistemas@gmail.com>
2018-11-13 15:34:55 -0500

    first commit and last

---
+++ Blog.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="js/jquery-3.2.1.min.js"></script>
+	<script type="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/publicstyles.css">
+</head>
+<body>
+
+<div style="height: 10px; background: #27aae1;"></div>
+<nav class="navbar navbar-dark bg-dark">
+		<div class="container">
+			<div class="row">	
+				<div class="navbar-header col-sm-2">
+					<a class="navbar-brand" href="Blog.php">
+						<img src="images/jazebakramcom.png" width="300" height="40">
+					</a>
+				</div>
+			</div>
+		<div>
+			<div class="row">	
+				<div class="col-sm-10">
+					<ul class="nav" >
+						<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Services</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Features</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+</nav>
+
+
+
+
+</body>
+</html>
